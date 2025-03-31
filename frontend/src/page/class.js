7431ebec9bf0fb93d943d78c6f990697632b18ac
Mori Meru <143690709+merumori@users.noreply.github.com>
2025-03-31 17:20:46 +0530

    Update class.js

--- frontend/src/page/class.js
+++ frontend/src/page/class.js
@@ -9,7 +9,7 @@ const Classes = () => {
   useEffect(() => {
     const fetchClasses = async () => {
       try {
-        const response = await axios.get('http://localhost:5000/api/classes');
+        const response = await axios.get('https://arjun-academy-ijou.onrender.com/api/classes');
         setClasses(response.data);
       } catch (error) {
         console.error('Error fetching classes:', error);
